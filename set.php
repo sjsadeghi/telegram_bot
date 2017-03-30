1e289c54dad3924f5b09b8be18f4e583f06b4843
sajjad sadeghi <sjsadeghi.dev@gmail.com>
2017-03-30 16:17:04 +0430

    set chat_id

--- set.php
+++ set.php
@@ -15,7 +15,7 @@ $BOT_NAME = 'EnAmuzanBot';
 $hook_url = 'https://bot-mytelegrambot.44fs.preview.openshiftapps.com/hook.php';
 try {
     $telegram = new Longman\TelegramBot\Telegram($API_KEY, $BOT_NAME);
-    $chat_id = '@sj_sadeghi';
+    $chat_id = 'https://t.me/sj_sadeghi';
     // Set Webhook
     $result = Request::sendMessage(['chat_id' => $chat_id, 'text' => 'Hi dear sajjad']);
     echo "result of send message is : " . $result;
